6953c6ac54e87894a29735d48fad453168689f8e
Theodore Hammar <theodore.hammar@elev.ga.ntig.se>
2023-09-06 10:39:17 +0200

    Uppgift B

--- src/App.java
+++ src/App.java
@@ -1,5 +1,6 @@
 public class App {
     public static void main(String[] args) throws Exception {
+        System.out.println("Uppgift A");
         int tal1 = 23;
         int tal2 = 45;
         int summa;
@@ -9,6 +10,18 @@ public class App {
         
         summa=tal1-tal2;
         System.out.println(tal1+"-"+tal2+"="+summa);
+
+        System.out.println("Uppgift B");
+
+       
+        int tal3=234232;
+        int tal4=3424;
+        produkt=tal3*tal4;
+        System.out.println(tal3+"*"+tal4+"="+produkt);
+        produkt=(tal2)*(tal3)*(tal4);
+        System.out.println(tal2+"*"+tal3+"*"+tal4+"="+produkt);
+
+
 
     }
 }
